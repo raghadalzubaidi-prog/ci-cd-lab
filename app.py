0648956896a149590e75f0f1188edf57e01ef44d
Raghad Alzubaidi <raghad.alzubaidi@isolution.sa>
2026-08-04 00:31:23 +0300

    Initial CI/CD Lab

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+VERSION = "v1 - hello from CI"
+
+@app.route("/")
+def home():
+    return VERSION
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
